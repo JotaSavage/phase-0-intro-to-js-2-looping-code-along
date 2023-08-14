7b3a304065a362dd7ca0cc7a8a86729368bd5d42
Juan Lugo <juanlugodev@gmail.com>
2023-08-14 16:10:52 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+const thankYouMessages = writeCards(names, event);
+
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // Invokes console.log once for each number, counting down from the number provided to zero.
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  
+  
+  
+  
